2ae0fd797250192856293c2e47b34d60ba068b9e
User <User@DESKTOP-DNP2SKS>
2020-09-23 19:36:39 +0200

    mob - commit tercero

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ and open the template in the editor.
             echo "Hola Profe";
             echo "mob - commit inicial";# inicales del nombre
             echo "mama mia";
+            echo "segundo dia bro";
         ?>
     </body>
 </html>
